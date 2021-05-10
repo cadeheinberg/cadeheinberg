38e5498b0b8864964e772823619da5e0d9b17742
CadeHeinberg <cade.heinb@gmail.com>
2021-05-09 22:12:12 -0500

    Update serverstats.php

--- MinecraftServer/serverstats.php
+++ MinecraftServer/serverstats.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "127.0.0.1";
+$servername = "localhost";
 $myfile = fopen("../../reach.txt", "r") or die("Unable to open file!");
 $username = fgets($myfile);
 $password = fgets($myfile);
