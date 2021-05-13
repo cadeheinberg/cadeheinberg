a3f532b10ce7640bd115e4c96e4707758f6fa714
CadeHeinberg <cade.heinb@gmail.com>
2021-05-13 01:40:46 -0500

    summary

--- NodeReact/myfirstreact/src/App.js
+++ NodeReact/myfirstreact/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I am editing this line
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Summer extends React.Component {
+  render() {
+    return <h2>I wish it was Summer!</h2>;
+  }
 }
 
-export default App;
+export default Summer;
--- NodeReact/myfirstreact/src/index.js
+++ NodeReact/myfirstreact/src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Get a Component made in a different file
+//This one is called App.js
+import Summer from './App.js';
+ReactDOM.render(<Summer />, document.getElementById('winter'));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//create using JSX
+const myheading = <h1>Cade Heinberg's React Page</h1>
+//const myheading = React.createElement('h1', {}, 'I do not use JSX!');
+
+const myexpression = <h1>React is {5 + 5} times better with JSX</h1>;
+//React is 10 times better with JSX
+
+//IMPORTANT
+//When you have a bunch of elements, make sure there is ONE wrapper around them all
+//A div element works well
+const mydouble = (
+  <div>
+    <h1>I am a Header.</h1>
+    <h2>I am a smaller header.</h2>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const myelement = (
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+);
+
+const myinput = <input type="text" />;
+
+
+//Component and State
+//Component properties should be kept in an object called state
+//I think state is a field of Component
+class Car extends React.Component {
+  constructor() {
+    super();
+    this.state = {color: "red"};
+  }
+  render() {
+    return <h2>Hi, I am a {this.state.color} Car!</h2>;
+  }
+}
+ReactDOM.render(<Car />, document.getElementById('root'));
+
+
+//Component and Props
+class Dog extends React.Component {
+  render() {
+    return <h2>I am a {this.props.color} Dog!</h2>;
+  }
+}
+ReactDOM.render(<Dog color="green"/>, document.getElementById('dog'));
+
+
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Who lives in my Garage?</h1>
+      <Car />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Garage />, document.getElementById('garage'));
+
+ReactDOM.render(myheading, document.getElementById('heading'));
+ReactDOM.render(myelement, document.getElementById('table'));
