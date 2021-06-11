db7c7dbb1806db18ff12cfd6234065801ff1277c
CadeHeinberg <cade.heinb@gmail.com>
2021-06-10 23:09:47 -0500

    updates

--- NodeReact/myfirstmodule.js
+++
@@ -1,5 +0,0 @@
-//exports allow the function to be accessed
-//outside of this file as a module
-exports.myDateTime = function () {
-    return Date();
-  };
--- NodeReact/myfirstreact/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Summer from './ProfilePages/Home.js';
-
-ReactDOM.render(<Summer />, document.getElementById('root'));
