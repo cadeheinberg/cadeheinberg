ea8e967927bf415f39a0895f294f91d57eeaf7b4
CadeHeinberg <cade.heinb@gmail.com>
2021-07-11 13:59:40 -0500

    updates

---
+++ oldsite/MinecraftServer/serverstats.php
@@ -0,0 +1,68 @@
+<?php
+$servername = "127.0.0.1";
+$myfile = fopen("../../reach.txt", "r") or die("Unable to open file!");
+$username = fgets($myfile);
+$password = fgets($myfile);
+
+// Create connection
+// $conn->close();
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE IF NOT EXISTS myDB";
+if ($conn->query($sql) === TRUE) {
+    #echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+    exit();
+}
+
+$dbname = "myDB";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// sql to create table
+$sql = "CREATE TABLE IF NOT EXISTS MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+    
+if ($conn->query($sql) === TRUE) {
+    #echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// inserting with prepared statements
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $firstname, $lastname, $email);
+
+// set parameters and execute
+$firstname = "John";
+$lastname = "Doe";
+$email = "john@example.com";
+$stmt->execute();
+
+$firstname = "Mary";
+$lastname = "Moe";
+$email = "mary@example.com";
+$stmt->execute();
+
+$firstname = "Julie";
+$lastname = "Dooley";
+$email = "julie@example.com";
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
+
+$conn->close();
+?>
