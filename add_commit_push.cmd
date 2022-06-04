73e9e8472d38c34ee647aae5de2b99ac05a4e0ce
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2022-06-04 12:26:57 -0500

    test

--- add_commit_push.cmd
+++ add_commit_push.cmd
@@ -1,4 +1,4 @@
 git add -A
 git commit -m "test"
 git push
-ssh -i C:\\Users\\cadeh\\.ssh\\id_rsa cade_bambam62@35.225.65.136 -t "cd /var/www/cadeheinberg ; bash --login ; exit"
+ssh -i C:\\Users\\cadeh\\.ssh\\id_rsa cade_bambam62@35.225.65.136 -t "vi /var/www/cadeheinberg/pull_from_github.sh ; bash --login"
