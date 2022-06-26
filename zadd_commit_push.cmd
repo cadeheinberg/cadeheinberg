fc2b788c03dfa2ed2926fcf88806a1b0052d7f11
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2022-06-26 18:24:30 -0500

    Updating from VSCODE

--- zadd_commit_push.cmd
+++ zadd_commit_push.cmd
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "test"
+git commit -m "Updating from VSCODE"
 git push
-ssh -i C:\\Users\\cadeh\\.ssh\\id_rsa cade_bambam62@35.225.65.136 -t "bash /var/www/mysites/cadeheinberg/pull_from_github.sh ; bash --login"
+ssh -i C:\\Users\\Cade\\.ssh\\id_rsa cade_bambam62@35.225.65.136 -t "bash /var/www/mysites/cadeheinberg/pull_from_github.sh ; bash --login"
